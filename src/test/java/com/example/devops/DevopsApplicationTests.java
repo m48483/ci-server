00cef48f4435209d9f632626f083a199e8246944
m48483 <71882579+m48483@users.noreply.github.com>
2024-05-21 11:49:30 +0900

    h2

--- src/test/java/com/example/devops/DevopsApplicationTests.java
+++ src/test/java/com/example/devops/DevopsApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.devops;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 @SpringBootTest
 class DevopsApplicationTests {
-
+	@Value("${spring.datasource.username}")
+	String username;
 	@Test
 	void contextLoads() {
 	}
-
+	@Test
+	void dbUsernameIsSa(){
+		Assertions.assertEquals("sa",username);
+	}
 }
